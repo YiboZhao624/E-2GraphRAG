b297aa928cf6e3f6aed818134c008988a4fb6b64
Yibo Zhao <51275903033@stu.ecnu.edu.cn>
2025-02-28 07:31:41 +0000

    update the method of loading the LLM

--- process_utils.py
+++ process_utils.py
@@ -10,17 +10,20 @@ def clean_cuda_memory(device_id):
             torch.cuda.empty_cache()
 
 def build_tree_task(args):
-    llm_path, llm_device, text, cache_folder, tokenizer_name, length, overlap, merge_num = args
+    llm_path, llm_device, text, cache_folder, tokenizer_name, length, overlap, merge_num, torch_dtype = args
     try:
         device_id = int(llm_device.split(':')[1]) if ':' in llm_device else 0
         
         # Load model and tokenizer in subprocess
-        llm = AutoModel.from_pretrained(llm_path)
-        if "Qwen2Model" in str(type(llm)):
-            from transformers import Qwen2ForCausalLM
-            llm = Qwen2ForCausalLM.from_pretrained(llm_path)
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-        llm_pipeline = pipeline("text-generation", model=llm, tokenizer=tokenizer, device=llm_device, max_new_tokens = 1200)
+        llm_pipeline = pipeline(
+            "text-generation", 
+            model=llm_path, 
+            tokenizer=tokenizer, 
+            device=llm_device, 
+            torch_dtype=torch_dtype,
+            max_new_tokens=1200
+        )
         
         # Process
         result, time_cost = build_tree(text, llm_pipeline, cache_folder, tokenizer, length, overlap, merge_num)
@@ -37,7 +40,6 @@ def build_tree_task(args):
     finally:
         # Clean up
         del llm_pipeline
-        del llm
         del tokenizer
         clean_cuda_memory(device_id)
 
