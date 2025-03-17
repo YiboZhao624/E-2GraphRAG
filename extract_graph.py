c42c209810553818237215acfba4fbefcef89a5e
Yibo Zhao <51275903033@stu.ecnu.edu.cn>
2025-03-17 08:10:30 +0000

    add the appearance_count

--- extract_graph.py
+++ extract_graph.py
@@ -34,6 +34,7 @@ def naive_extract_graph(text:str, nlp:spacy.Language):
 
     # process the name like John Brown
     double_nouns = {}
+    appearance_count = {}
 
     # TODO: 一个chunk里的连通还是一个句子里连通？
     for sent in doc.sents:
@@ -48,12 +49,16 @@ def naive_extract_graph(text:str, nlp:spacy.Language):
                     for name in name_parts:
                         double_nouns[name] = name_parts
                     sentence_terms.extend(name_parts)
+                    for name in name_parts:
+                        appearance_count[name] = appearance_count.get(name, 0) + 1
                 else:
                     sentence_terms.append(ent.text)
-
+                    appearance_count[ent.text] = appearance_count.get(ent.text, 0) + 1
+            
             # process the organization or country.
             elif ent.label_ in ["ORG", "GPE"]:
                 sentence_terms.append(ent.text)
+                appearance_count[ent.text] = appearance_count.get(ent.text, 0) + 1
             for token in ent:
                 ent_positions.add(token.i)
 
@@ -62,11 +67,14 @@ def naive_extract_graph(text:str, nlp:spacy.Language):
                 continue
             if token.pos_ == "NOUN" and token.lemma_.strip():
                 sentence_terms.append(token.lemma_.lower())
+                appearance_count[token.lemma_.lower()] = appearance_count.get(token.lemma_.lower(), 0) + 1
             elif token.pos_ == "PROPN" and token.text.strip():
                 sentence_terms.append(token.lemma_.lower())
+                appearance_count[token.lemma_.lower()] = appearance_count.get(token.lemma_.lower(), 0) + 1
             elif token.pos_ == "PROPN" and token.text.strip():
                 sentence_terms.append(token.text)
-        
+                appearance_count[token.text] = appearance_count.get(token.text, 0) + 1
+                
         all_nouns.update(sentence_terms)
         
         # Count the cooccurrence of terms
@@ -79,7 +87,8 @@ def naive_extract_graph(text:str, nlp:spacy.Language):
     return {
         "nouns": list(all_nouns),
         "cooccurrence": noun_pairs,
-        "double_nouns": double_nouns
+        "double_nouns": double_nouns,
+        "appearance_count": appearance_count
     }
 
 
@@ -215,10 +224,12 @@ def merge_entities(nouns: List[str], eps: float = 0.15, min_samples: int = 2, de
 def load_cache(cache_path:str):
     graph_file_path = os.path.join(cache_path, "graph.json")
     index_file_path = os.path.join(cache_path, "index.json")
+    appearance_count_file_path = os.path.join(cache_path, "appearance_count.json")
     edges = json.load(open(graph_file_path, "r"))
     index = json.load(open(index_file_path, "r"))
+    appearance_count = json.load(open(appearance_count_file_path, "r"))
     graph = build_graph(edges)
-    return graph, index
+    return graph, index, appearance_count
 
 def save_graph(result, cache_path:str):
     with open(cache_path, "w") as f:
@@ -227,26 +238,35 @@ def save_graph(result, cache_path:str):
 def save_index(result, cache_path:str):
     with open(cache_path, "w") as f:
         json.dump(result, f, indent=4)
+
+def save_appearance_count(result, cache_path:str):
+    with open(cache_path, "w") as f:
+        json.dump(result, f, indent=4)
     
-def extract_graph(text:List[str], cache_folder:str, nlp:spacy.Language):
+def extract_graph(text:List[str], cache_folder:str, nlp:spacy.Language, use_cache=True, reextract=False):
     extract_start_time = time.time()
-    if os.path.exists(os.path.join(cache_folder, "graph.json")) and os.path.exists(os.path.join(cache_folder, "index.json")):
+    if use_cache and os.path.exists(os.path.join(cache_folder, "graph.json")) and os.path.exists(os.path.join(cache_folder, "index.json")) and os.path.exists(os.path.join(cache_folder, "appearance_count.json")):
         return load_cache(cache_folder), -1
     else:
         graph_file_path = os.path.join(cache_folder, "graph.json")
         index_file_path = os.path.join(cache_folder, "index.json")
-
+        appearance_count_file_path = os.path.join(cache_folder, "appearance_count.json")
         edges = []
         index = {}
+        appearance_count = {}
 
         for i, chunk in enumerate(text):
             naive_result = naive_extract_graph(chunk, nlp)
             # not merge the entities.
+            appearance_count["leaf_{}".format(i)] = naive_result["appearance_count"]
 
             for noun in naive_result["nouns"]:
                 if noun not in index:
                     index[noun] = []
                 index[noun].append("leaf_{}".format(i))
+            
+            for noun, count in naive_result["appearance_count"].items():
+                appearance_count[noun] = appearance_count.get(noun, 0) + count
 
             # add the cooccurrence.
             for pair, weight in naive_result["cooccurrence"].items():
@@ -255,12 +275,15 @@ def extract_graph(text:List[str], cache_folder:str, nlp:spacy.Language):
 
         # build the graph.
         G = build_graph(edges)
-
+        if reextract == True:
+            save_appearance_count(appearance_count, appearance_count_file_path)
+            return (G, index, appearance_count), -1
         # save the graph and index.
         save_graph(edges, graph_file_path)
         save_index(index, index_file_path)
+        save_appearance_count(appearance_count, appearance_count_file_path)
         extract_end_time = time.time()
-        return (G, index), extract_end_time - extract_start_time
+        return (G, index, appearance_count), extract_end_time - extract_start_time
 
 if __name__ == "__main__":
     # nouns = [
