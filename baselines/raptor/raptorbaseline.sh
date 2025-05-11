340b24423458237d56b8b6c08017696c00971156
yibo zhao <90241849+YiboZhao624@users.noreply.github.com>
2025-05-11 19:45:54 +0800

    Delete baselines directory

--- baselines/raptor/raptorbaseline.sh
+++
@@ -1,8 +0,0 @@
-python raptorbaseline.py \
-    --dataset NarrativeQA \
-    --output_folder /root/projects/lightTAG/NarrativeQA/Raptor \
-    --llm_name /root/shared_planing/LLM_model/Qwen2.5-14B-Instruct \
-    --embedding_model_name BAAI/bge-m3 \
-    --qa_device cuda:0 \
-    --summary_device cuda:2 \
-    --embedding_device cuda:6 \
